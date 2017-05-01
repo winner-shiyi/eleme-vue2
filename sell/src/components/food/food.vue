b8cd8fe98f633bbe077fd9603e326ff45443551f
weina <313614833@qq.com>
2017-05-01 18:32:22 +0800

    完成商品评价的样式

--- sell/src/components/food/food.vue
+++ sell/src/components/food/food.vue
@@ -34,6 +34,21 @@
         <h1 class="title">商品评价</h1>
         <v-ratingselect :select-type="selectType" 
         :only-content="onlyContent" :desc="desc" :ratings="food.ratings"></v-ratingselect>
+        <div class="rating-wrapper">
+          <ul v-show="food.ratings && food.ratings.length">
+            <li v-show="needShow(rating.rateType,rating.text)" v-for="rating in food.ratings" class="rating-item border-1px-bottom">
+              <div class="user">
+                <span class="name">{{rating.username}}</span>
+                <img :src="rating.avatar" alt="" class="avatar" width="12" height="12">
+              </div>
+              <div class="time">{{rating.rateTime|formatDate}}</div>
+              <p class="text">
+                <span :class="{'icon-thumb_up':rating.rateType===0,'icon-thumb_down':rating.rateType===1}"></span>{{rating.text}}
+              </p>
+            </li>
+          </ul>
+          <div class="no-rating" v-show="!food.ratings || !food.ratings.length">暂无评价</div>
+        </div>
       </div>
     </div>
   </div>
@@ -42,6 +57,7 @@
 <script>
 import BScroll from 'better-scroll'
 import Vue from 'vue'
+import {formatDate} from 'common/js/date'
 import cartcontrol from 'components/cartcontrol/cartcontrol'
 import split from 'components/split/split'
 import ratingselect from 'components/ratingselect/ratingselect'
@@ -91,6 +107,36 @@ export default {
       this.$dispatch('cart.add', event.target)
       // this.food.count = 1 下面的代码等于这句话
       Vue.set(this.food, 'count', 1)
+    },
+    needShow (type, text) {
+      if (this.onlyContent && !text) {
+        return false
+      }
+      if (this.selectType === ALL) {
+        return true
+      } else {
+        return type === this.selectType
+      }
+    }
+  },
+  events: {
+    'ratingtype.select' (type) {
+      this.selectType = type
+      this.$nextTick(() => {
+        this.scroll.refresh()
+      })
+    },
+    'content.toggle' (onlyContent) {
+      this.onlyContent = onlyContent
+      this.$nextTick(() => {
+        this.scroll.refresh()
+      })
+    }
+  },
+  filters: {
+    formatDate (time) {
+      let date = new Date(time)
+      return formatDate(date, 'yyyy-MM-dd hh:mm')
     }
   },
   components: {
@@ -102,6 +148,8 @@ export default {
 </script>
 
 <style lang="stylus">
+  @import "../../common/stylus/mixin.styl"
+
   .food{
     position:fixed
     left:0
@@ -227,6 +275,57 @@ export default {
         margin-left:18px
         font-size:14px
         color:rgb(7,17,27)
+      }
+      .rating-wrapper{
+        padding:0 18px
+        .rating-item{
+          position:relative
+          padding:16px 0
+          border-1px-bottom(rgba(7,17,27,.1))
+          .user{
+            position:absolute
+            right:0
+            top:16px
+            line-height:12px
+            font-size:0
+            .name{
+              display:inline-block
+              vertical-align:top
+              margin-right:6px
+              font-size:10px
+              color:rgb(147,153,159)
+            }
+            .avatar{
+              border-radius:50%
+            }
+          }
+          .time{
+            margin-bottom:6px
+            line-height:12px
+            font-size:10px
+            color:rgb(147,153,159)
+          }
+          .text{
+            line-height:16px
+            font-size:12px
+            color:rgb(7,17,27)
+            .icon-thumb_down,.icon-thumb_up{
+              margin-right:4px
+              font-size:12px
+            }
+            .icon-thumb_up{
+              color:rgb(0,160,220)
+            }
+            .icon-thumb_down{
+              color:rgb(147,153,159)
+            }
+          }
+        }
+        .no-rating{
+          padding:16px 0
+          font-size:12px
+          color:rgb(147,153,159)
+        }
       }
     }
   }
