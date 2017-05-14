02543903734b44ed574588defe8ecf7287535f3c
weina <313614833@qq.com>
2017-05-15 00:45:21 +0800

    更新右侧商品信息样式

--- sell/src/components/goods/goods.vue
+++ sell/src/components/goods/goods.vue
@@ -3,8 +3,8 @@
     <!--左侧-->
     <div class="menu-wrapper">
       <ul>
-        <li v-for="item in goods">
-          <span class="text">
+        <li v-for="item in goods" class="menu-item">
+          <span class="text border-1px-bottom">
             <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
           </span>
         </li>
@@ -12,7 +12,30 @@
     </div>
     <!--右侧-->
     <div class="foods-wrapper">
-
+      <ul>
+        <li v-for="item in goods" class="food-list">
+          <h1 class="title">{{item.name}}</h1>
+          <ul>
+            <li v-for="food in item.foods" class="food-item border-1px-bottom">
+              <div class="icon">
+                <img :src="food.icon" alt="" width="57" height="57">
+              </div>
+              <div class="content">
+                <h2 class="name">{{food.name}}</h2>
+                <p v-show="food.description" class="desc">{{food.description}}</p>
+                <div class="extra">
+                  <span class="count">月售{{food.sellCount}}份</span>
+                  <span>好评率{{food.rating}}</span>
+                </div>
+                <div class="price">
+                  <span class="now">¥{{food.price}}</span>
+                  <span class="old" v-show="food.oldPrice">¥{{food.oldPrice}}</span>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
@@ -46,6 +69,8 @@ export default {
 </script>
 
 <style lang="stylus">
+  @import "../../common/stylus/mixin.styl"
+
   .goods{
     display:flex
     position:absolute
@@ -57,8 +82,106 @@ export default {
       flex:0 0 80px
       width:80px
       background:#f3f5f7
-      .foods-wrapper{
-        flex:1
+      .menu-item{
+        display:table
+        width:56px
+        height:54px
+        padding:0 12px
+        line-height:14px
+        .icon{
+          display:inline-block
+          vertical-align:top
+          width:12px
+          height:12px
+          margin-right:2px
+          background-size:12px 12px
+          background-repeat:no-repeat
+          &.decrease{
+            bg-image('decrease_3')
+          }
+          &.discount{
+            bg-image('discount_3')
+          }
+          &.guarantee{
+            bg-image('guarantee_3')
+          }
+          &.invoice{
+            bg-image('invoice_3')
+          }
+          &.special{
+            bg-image('special_3')
+          }
+        }
+        .text{
+          display:table-cell
+          width:56px
+          vertical-align:middle
+          font-size:12px
+          border-1px-bottom(rgba(7,17,27,.1))
+        }
+      }
+    }
+    .foods-wrapper{
+      flex:1  
+      .title{
+        padding-left:14px
+        height:26px
+        line-height:26px
+        border-left:2px solid #d9dde1
+        font-size:12px
+        color:rgb(147,153,159)
+        background:#f3f5f7
+      }
+      .food-item{
+        display:flex
+        margin:18px 18px 0
+        padding-bottom:18px
+        border-1px-bottom(rgba(7,17,27,.1))
+        &:last-child{
+          border-none()
+        }
+        .icon{
+          flex:0 0 57px
+          width:57px
+          margin-right:10px
+        }
+        .content{
+          flex:1
+          .name{
+            margin:2px 0 8px
+            height:14px
+            line-height:14px
+            font-size:14px
+            color:rgb(7,17,27)
+          }
+          .desc,.extra{
+            line-height:10px
+            font-size:10px
+            color:rgb(147,153,159)
+          }
+          .desc{
+            margin-bottom:8px
+          }
+          .extra{
+            .count{
+              margin-right:12px
+            }
+          }
+          .price{
+            font-weight:700
+            line-height:24px
+            .now{
+              margin-right:8px
+              font-size:14px
+              color:rgb(240,20,20)
+            }
+            .old{
+              text-decoration:line-through
+              font-size:10px
+              color:rgb(147,153,159)
+            }
+          }
+        }
       }
     }
   }
